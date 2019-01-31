f4153abaed3468d42a8fee37fe59d1ee020d9808
facc <830479@stud.unive.it>
2019-01-31 21:51:20 +0100

    inixio primo bottone

--- app/src/main/java/com/example/michele/bozze/Sezioni/ManualFragment.java
+++ app/src/main/java/com/example/michele/bozze/Sezioni/ManualFragment.java
@@ -2,18 +2,46 @@ package com.example.michele.bozze.Sezioni;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.michele.bozze.Data.GlobalVariables;
 import com.example.michele.bozze.R;
 
 public class ManualFragment extends Fragment {
+
+
+    private static final String TAG  = "MANUAL FRAGMENT";
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_manual_controls, container, false);
 
         GlobalVariables g = (GlobalVariables)(getActivity().getApplication());
+
+        Log.e(TAG, "inizio a creare Button");
+        ImageButton frecciaDX = rootView.findViewById(R.id.arrow_right);
+        ImageButton frecciaSX = rootView.findViewById(R.id.arrow_left);
+        ImageButton frecciaAV = rootView.findViewById(R.id.arrow_up);
+        ImageButton frecciaIN = rootView.findViewById(R.id.arrow_down);
+
+        Log.e(TAG, "setup ontouch listener");
+        frecciaDX.setOnTouchListener(new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+                //g.useBluetooth().muoviAvanti();
+
+            } else if (event.getAction() == MotionEvent.ACTION_BUTTON_RELEASE) {
+                // g.useBluetooth().fermati();
+            }//NON SEMBRA RICONOSCERE ACTION DOWN
+            return false;
+        }
+    });
 
         return rootView;
     }
